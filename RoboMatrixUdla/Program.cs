79f96fb57a514dd03504f09128fbe9e1948ea933
JuanMaViana <jnviana@udlanet.ec>
2017-03-04 19:20:35 -0500

    Creacion MDI

--- RoboMatrixUdla/Program.cs
+++ RoboMatrixUdla/Program.cs
@@ -16,7 +16,7 @@ namespace RoboMatrixUdla
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMDI());
         }
     }
 }
