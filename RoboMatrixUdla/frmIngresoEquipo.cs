54ce09eb83363fa996d2e7d6ffcc4f8cedfb4a64
JuanMaViana <jnviana@udlanet.ec>
2017-03-04 22:38:40 -0500

    Ingreso Equipo

---
+++ RoboMatrixUdla/frmIngresoEquipo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoboMatrixUdla
+{
+    public partial class frmIngresoEquipo : Form
+    {
+        public frmIngresoEquipo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Archivo JPG|*.jpg";
+
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pcbLogo.Image = Image.FromFile(fileDialog.FileName);
+            }
+        }
+    }
+}
