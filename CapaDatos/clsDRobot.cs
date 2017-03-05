66944148824dccb780e6bee59b171a368e966b91
Paulo <Paulo@LAPTOP-ALEJO>
2017-03-05 01:07:09 -0500

    CRUD Robot

---
+++ CapaDatos/clsDRobot.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class clsDRobot
+    {
+        MERRobotDataContext bd = new MERRobotDataContext();
+
+        public object D_consultaRobot()
+        {
+            try
+            {
+                var rob1 = from r in bd.tTblRobot
+                           select new { r.idRobot, r.nomRobot, r.estadoRobot };
+                return rob1;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool D_ingresaRobot(string nombre, string estado)
+        {
+            try
+            {
+                TblRobot rob1 = new TblRobot { nomRobot = nombre, estadoRobot = estado };
+                bd.TblRobots.InsertOnSubmit(rob1);
+                bd.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+
+            }
+        }
+
+        public bool D_ActualizaRobot(int id, string nombre, string estado)
+        {
+            try
+            {
+                TblRobot rob1 = bd.TblRobot.First(r => r.idRobot == id);
+                rob1.nomRobot = nombre;
+                rob1.estadoRobot = estado;
+                bd.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+
+            }
+        }
+
+        public DataSet D_consultaRobot(int idRobot)
+        {
+            DataSet ds2 = new DataSet();
+            SqlDataAdapter adaptador;
+
+            clsConexion.abrirConexion();
+
+            string sql = "select * from TblRobot where idRobot=" + idRobot;
+
+            adaptador = new SqlDataAdapter(sql, clsConexion.conexion);
+
+            adaptador.Fill(ds2, "TblRobot");
+
+            clsConexion.cerrarConexion();
+
+            return ds2;
+        }
+
+        public bool D_EliminarRobot(int id)
+        {
+            try
+            {
+                TblRobot rob1 = bd.TblRobot.First(r => r.idRobot == id);
+
+                bd.TblRobot.DeleteOnSubmit(rob1);
+                bd.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+
+            }
+        }
+    }
+}
---
+++ CapaNegocio/clsNRobot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class clsNRobot
+    {
+        public int idRobot { get; set; }
+        public string Nombre { get; set; }
+        public string Estado { get; set; }
+
+        clsDRobot D_objRobot = new clsDRobot();
+        public object N_consultaRobot()
+        {
+            var rob1 = D_objRobot.D_consultaRobot();
+            return rob1;
+        }
+
+        public bool N_IngresarRobot(string nombre, string estado)
+        {
+            Nombre = nombre;
+            Estado = estado;
+            if (D_objRobot.D_ingresaRobot(Nombre, Estado))
+                return true;
+            else
+                return false;
+        }
+
+        public bool N_ActualizarRobot(int id, string nombre, string estado)
+        {
+            idRobot = id;
+            Nombre = nombre;
+            Estado = estado;
+            if (D_objRobot.D_ActualizaRobot(idRobot, Nombre, Estado))
+                return true;
+            else
+                return false;
+        }
+
+        public bool N_EliminarRobot(int id)
+        {
+            idRobot = id;
+
+            return D_objRobot.D_EliminarRobot(idRobot);
+        }
+        public DataSet N_consultaRobot(int id)
+        {
+            idRobot = id;
+            return D_objRobot.D_consultaRobot(idRobot);
+        }
+    }
+}
