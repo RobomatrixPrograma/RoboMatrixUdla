5d902a2c53d3f778b7b2e509ff7af716a3ca66b3
JuanMaViana <jnviana@udlanet.ec>
2017-03-09 03:58:49 -0500

    Juan Adaptaciones

--- CapaDatos/clsD_Megasumo.cs
+++ CapaDatos/clsD_Megasumo.cs
@@ -9,13 +9,13 @@ namespace CapaDatos
     public class clsD_Megasumo
     {
         MERRobotDataContext bd = new MERRobotDataContext();
-        public bool agregarBatalla(int robot1, string nom1, int robot2, string nomRobot2)
+        public bool agregarBatalla(int robot1, int robot2)
         {
 
             try
             {
                 TblMegasumo megas = new TblMegasumo { idRobotUno = robot1, idRobotDos = robot2, };
-                bd.TblMegasumo.InsertOnSubmit(megas);
+                bd.TblMegasumos.InsertOnSubmit(megas);
                 bd.SubmitChanges();
                 return true;
             }
@@ -23,6 +23,21 @@ namespace CapaDatos
             {
                 return false;
             }
+        }
+
+        public object consultaMegasumo()
+        {
+            try
+            {
+                var megas = from m in bd.TblMegasumos
+                            select new { m.idBatalla_megasumo };
+                return megas;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+
         }
     }
 }
--- CapaNegocio/clsN_Llaves.cs
+++ CapaNegocio/clsN_Llaves.cs
@@ -16,35 +16,48 @@ namespace CapaNegocio
         public int robot2 { get; set; }
         public string nomRobot1 { get; set; }
         public string nomRobot2 { get; set; }
-        public bool N_ingresarCategoria(string cat, int idr1, int idr2, string nom1, string nom2)
+        public bool N_ingresarCategoria(int cat, int idr1, int idr2)
         {
             robot1 = idr1;
             robot2 = idr2;
-            nomRobot1 = nom1;
-            nomRobot2 = nom2;
+            Console.WriteLine(cat);
             switch(cat)
             {
-                case "Minisumo":
-                    if (D_Minisumo.agregarBatalla(robot1, nom1, robot2, nomRobot2))
+                case 2:
+                    if (D_Minisumo.agregarBatalla(robot1, robot2))
+                        return true;
+                    else
+                        return false; 
+                case 1:
+                    if (D_Megasumo.agregarBatalla(robot1, robot2))
                         return true;
                     else
                         return false;
-                    break;
-                case "Megasumo":
-                    if (D_Megasumo.agregarBatalla(robot1, nom1, robot2, nomRobot2))
+                case 3:
+                    if (D_Sumolego.agregarBatalla(robot1, robot2))
                         return true;
                     else
                         return false;
-                    break;
-                case "Sumo Lego":
-                    if (D_Sumolego.agregarBatalla(robot1, nom1, robot2, nomRobot2))
-                        return true;
-                    else
-                        return false;
-                    break;
                 default:
                     return false;
-                    break;
+            }
+        }
+
+        public object consultaCategorias(int cat)
+        {
+            switch (cat)
+            {
+                case 2:
+                    var minis = D_Minisumo.consultaMinisumo();
+                        return minis;
+                case 1:
+                    var megas = D_Megasumo.consultaMegasumo();
+                        return megas;
+                case 3:
+                    var legos = D_Sumolego.consultaLegoSumo();
+                        return legos;
+                default:
+                    return null;
             }
         }
     }
