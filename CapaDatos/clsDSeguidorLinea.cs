e7a8c8b98b5681375ba8715d1ee1985e7e2b1862
Paulo <Paulo@LAPTOP-ALEJO>
2017-03-09 13:31:23 -0500

    Seguidor de linea

---
+++ CapaDatos/clsDSeguidorLinea.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class clsDSeguidorLinea
+    {
+        MERRobotDataContext bd = new MERRobotDataContext();
+        public object D_consultaLista()
+        {
+            try
+            {
+                var rob1 = from r in bd.TblSeguidorLineas
+                           where r.estado != "SI"
+                           select new { r.idRobot, r.TblRobot.nomRobot };
+                return rob1;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int D_consultaListaID()
+        {
+            try
+            {
+                int a = (from r in bd.TblSeguidorLineas
+                         where r.estado != "SI"
+                         select new { r.idRobot }).FirstOrDefault().idRobot;
+
+                return a;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool D_ActualizarEstado(int id, string estado)
+        {
+            try
+            {
+                TblSeguidorLinea lab1 = bd.TblSeguidorLineas.First(r => r.idBatalla_SeguidorLinea == id);
+                lab1.estado = estado;
+                bd.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+
+            }
+        }
+
+        public string D_consultaListaParticipante()
+        {
+            try
+            {
+                string rob1 = (from r in bd.TblSeguidorLineas
+                               where r.estado != "SI"
+                               select new { r.TblRobot.nomRobot }).FirstOrDefault().nomRobot;
+
+                return rob1;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public string D_consultaListaParticipante2()
+        {
+            try
+            {
+                int a = (from r in bd.TblSeguidorLineas
+                         select new { r.idRobot }).FirstOrDefault().idRobot;
+                string rob1 = (from r in bd.TblRobots
+                               where r.idRobot == a
+                               select new { r.TblEquipo.nomEquipo }).FirstOrDefault().nomEquipo;
+                return rob1;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public int D_consultaListaIDBatalla()
+        {
+            try
+            {
+                int a = (from r in bd.TblSeguidorLineas
+                         where r.estado != "SI"
+                         select new { r.idBatalla_SeguidorLinea }).FirstOrDefault().idBatalla_SeguidorLinea;
+
+                return a;
+            }
+            catch
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ CapaNegocio/clsNPuntajeSeguidorLinea.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+namespace CapaNegocio
+{
+    public class clsNPuntajeSeguidorLinea
+    {
+        clsDPuntajeSeguidorLinea D_objPuntaje = new clsDPuntajeSeguidorLinea();
+
+        int idRobot { get; set; }
+        decimal tiempo1 { get; set; }
+        decimal tiempo2 { get; set; }
+        decimal tiempo3 { get; set; }
+        decimal mejor { get; set; }
+        public object N_consultaLista()
+        {
+            var rob1 = D_objPuntaje.D_consultaLista();
+            return rob1;
+        }
+
+        public bool N_IngresarPuntaje(int id, decimal t1, decimal t2, decimal t3)
+        {
+            idRobot = id;
+            tiempo1 = t1;
+            tiempo2 = t2;
+            tiempo3 = t3;
+            if (tiempo1 <= tiempo2 && tiempo1 <= tiempo3)
+            {
+                mejor = tiempo1;
+            }
+            else if (tiempo2 <= tiempo3)
+            {
+                mejor = tiempo2;
+            }
+            else
+            {
+                mejor = tiempo3;
+            }
+            return D_objPuntaje.D_ingresarPuntaje(idRobot, tiempo1, tiempo2, tiempo3, mejor);
+        }
+    }
+}
