9b73dc8ae4155ceb225463bba236cb1604403709
JuanMaViana <jnviana@udlanet.ec>
2017-03-09 09:50:11 -0500

    Cambios MDI

--- RoboMatrixUdla/frmMDI.Designer.cs
+++ RoboMatrixUdla/frmMDI.Designer.cs
@@ -40,6 +40,13 @@
             this.ordenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.oToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minisumoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.megasumoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.legosumoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seguidorDeLíneaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seguidorDeLíneaLegoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laberintoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dronToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -73,7 +80,14 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.configuraciónToolStripMenuItem});
+            this.configuraciónToolStripMenuItem,
+            this.minisumoToolStripMenuItem,
+            this.megasumoToolStripMenuItem,
+            this.legosumoToolStripMenuItem,
+            this.seguidorDeLíneaToolStripMenuItem,
+            this.seguidorDeLíneaLegoToolStripMenuItem,
+            this.laberintoToolStripMenuItem,
+            this.dronToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(1205, 24);
@@ -93,14 +107,14 @@
             // ingresarRobotToolStripMenuItem1
             // 
             this.ingresarRobotToolStripMenuItem1.Name = "ingresarRobotToolStripMenuItem1";
-            this.ingresarRobotToolStripMenuItem1.Size = new System.Drawing.Size(198, 22);
+            this.ingresarRobotToolStripMenuItem1.Size = new System.Drawing.Size(191, 22);
             this.ingresarRobotToolStripMenuItem1.Text = "Ingresar Equipo";
             this.ingresarRobotToolStripMenuItem1.Click += new System.EventHandler(this.ingresarRobotToolStripMenuItem1_Click);
             // 
             // ingresarRobotToolStripMenuItem2
             // 
             this.ingresarRobotToolStripMenuItem2.Name = "ingresarRobotToolStripMenuItem2";
-            this.ingresarRobotToolStripMenuItem2.Size = new System.Drawing.Size(198, 22);
+            this.ingresarRobotToolStripMenuItem2.Size = new System.Drawing.Size(191, 22);
             this.ingresarRobotToolStripMenuItem2.Text = "Ingresar Robot";
             this.ingresarRobotToolStripMenuItem2.Click += new System.EventHandler(this.ingresarRobotToolStripMenuItem2_Click);
             // 
@@ -126,6 +140,52 @@
             this.oToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.oToolStripMenuItem.Text = "Ingresar Orden";
             this.oToolStripMenuItem.Click += new System.EventHandler(this.oToolStripMenuItem_Click);
+            // 
+            // minisumoToolStripMenuItem
+            // 
+            this.minisumoToolStripMenuItem.Name = "minisumoToolStripMenuItem";
+            this.minisumoToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.minisumoToolStripMenuItem.Text = "Minisumo";
+            this.minisumoToolStripMenuItem.Click += new System.EventHandler(this.minisumoToolStripMenuItem_Click);
+            // 
+            // megasumoToolStripMenuItem
+            // 
+            this.megasumoToolStripMenuItem.Name = "megasumoToolStripMenuItem";
+            this.megasumoToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.megasumoToolStripMenuItem.Text = "Megasumo";
+            // 
+            // legosumoToolStripMenuItem
+            // 
+            this.legosumoToolStripMenuItem.Name = "legosumoToolStripMenuItem";
+            this.legosumoToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.legosumoToolStripMenuItem.Text = "Legosumo";
+            // 
+            // seguidorDeLíneaToolStripMenuItem
+            // 
+            this.seguidorDeLíneaToolStripMenuItem.Name = "seguidorDeLíneaToolStripMenuItem";
+            this.seguidorDeLíneaToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
+            this.seguidorDeLíneaToolStripMenuItem.Text = "Seguidor de Línea";
+            this.seguidorDeLíneaToolStripMenuItem.Click += new System.EventHandler(this.seguidorDeLíneaToolStripMenuItem_Click);
+            // 
+            // seguidorDeLíneaLegoToolStripMenuItem
+            // 
+            this.seguidorDeLíneaLegoToolStripMenuItem.Name = "seguidorDeLíneaLegoToolStripMenuItem";
+            this.seguidorDeLíneaLegoToolStripMenuItem.Size = new System.Drawing.Size(142, 20);
+            this.seguidorDeLíneaLegoToolStripMenuItem.Text = "Seguidor de Línea Lego";
+            this.seguidorDeLíneaLegoToolStripMenuItem.Click += new System.EventHandler(this.seguidorDeLíneaLegoToolStripMenuItem_Click);
+            // 
+            // laberintoToolStripMenuItem
+            // 
+            this.laberintoToolStripMenuItem.Name = "laberintoToolStripMenuItem";
+            this.laberintoToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.laberintoToolStripMenuItem.Text = "Laberinto";
+            this.laberintoToolStripMenuItem.Click += new System.EventHandler(this.laberintoToolStripMenuItem_Click);
+            // 
+            // dronToolStripMenuItem
+            // 
+            this.dronToolStripMenuItem.Name = "dronToolStripMenuItem";
+            this.dronToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.dronToolStripMenuItem.Text = "Dron";
             // 
             // frmMDI
             // 
@@ -163,6 +223,13 @@
         private System.Windows.Forms.ToolStripMenuItem ordenToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
         private System.Windows.Forms.ToolStripMenuItem oToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minisumoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem megasumoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem legosumoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seguidorDeLíneaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seguidorDeLíneaLegoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laberintoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dronToolStripMenuItem;
     }
 }
 
